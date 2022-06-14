be1e2b1b009ca6d54d9a0be6dd7c74df0fca2146
opentmn <102440180+opentmn@users.noreply.github.com>
2022-06-15 01:34:03 +0500

    finish JS ssylki

--- js/ssylki.js
+++ js/ssylki.js
@@ -1,28 +1,4 @@
-const s1 =
-  "https://drive.google.com/file/d/1QZcajVLgzlp6R1Q6GUFDzGeCVoKbatKi/view?usp=sharing";
-const ssylka1 = document.createElement("a");
-ssylka1.setAttribute("href", s1);
-
-const s2 =
-  "https://drive.google.com/drive/folders/1We4WH3OePMOlyY2Onmt1p7a80iw1iJPz?usp=sharing";
-const ssylka2 = document.createElement("a");
-ssylka2.setAttribute("href", s2);
-
-const s3 =
-  "https://drive.google.com/file/d/1YcuLbe-KxsDRvazRCYBahRCkwAITM0dd/view?usp=sharing";
-const ssylka3 = document.createElement("a");
-ssylka3.setAttribute("href", s3);
-
-// const s4 =
-//   "https://drive.google.com/file/d/1cSlGYV0KQe5J5Fk3g72LQulvtRVoROPy/view?usp=sharing";
-// const ssylka4 = document.createElement("a");
-// ssylka4.setAttribute("href", s4);
-
-const s5 =
-  "https://drive.google.com/file/d/1F5MHLOlniDvVqA_ZXOjNK3O60Y1m1c85/view?usp=sharing";
-const ssylka5 = document.createElement("a");
-ssylka5.setAttribute("href", s5);
-
-const workingLink = ssylka2;
+const workingLink = document.createElement("a");
+workingLink.setAttribute("href", "https://drive.google.com/drive/folders/1We4WH3OePMOlyY2Onmt1p7a80iw1iJPz?usp=sharing");
 
 export default workingLink;
